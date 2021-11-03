fd2121184cfc2b1ed2bd686828b5e8257a25e191
s.fedorovich <s.fedorovich@CZC71776GT>
2021-11-03 11:30:59 +0300

    Добавьте файлы проекта.

---
+++ Занятие 13/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Занятие_13
+{
+    class Program
+    // наследование класса Building
+    {
+        class Building
+        {
+            //Описание класса
+            string address;
+            double length;
+            double width;
+            double height;
+            
+            public Building (string address, double length, double wide, double height) // конструктор
+            {
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Building building = new Building(); //экземпляр класса Building
+
+
+
+
+        }
+    }
+}
